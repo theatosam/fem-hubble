eb8332e1dc6a9453cf9987a28b4087beeed1f96f
Ato Sam <atosam23@gmail.com>
2019-11-18 23:11:14 +0000

    Declare script variables

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-function validateForm() {
-  let firstName = document.forms.signUp.firstname.value;
-  if (firstName == '') {
+/*function validateForm() {
+  let firstName = document.getElementsById('firstname');
+  let lastName = document.getElementById('lastname');
+  let email = document.getElementById('email');
+  let password = document.getElementById('password');
+
+  if (firstName == '' || lastName == '' || email == '' || password == '') {
     alert('First Name cannot be empty');
     return false;
+  } else {
+    return true;
   }
-}
+}*/
+
+const firstName = document.getElementById('firstname');
+const lastName = document.getElementById('lastname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
