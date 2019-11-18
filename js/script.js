cba7d7f2f9bc8068838f5b84758a792ccee8e7bc
Ato Sam <atosam23@gmail.com>
2019-11-18 20:50:52 +0000

    Link script to html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-function errorMessage() {
-    if ()
+function validateForm() {
+  let firstName = document.forms.signUp.firstname.value;
+  if (firstName == '') {
+    alert('First Name cannot be empty');
+    return false;
+  }
 }
